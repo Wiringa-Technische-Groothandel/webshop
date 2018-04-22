48186009eb63c4a4624770cc0148346aaf8ef62d
Thomas Wiringa <thomas.wiringa@gmail.com>
2018-04-22 19:11:22 +0200

    Use FA 5 Pro CDN

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -9,6 +9,8 @@
 
 <title>WTG - @yield('title')</title>
 
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.0.10/css/all.css"
+      integrity="sha384-KwxQKNj2D0XKEW5O/Y6haRH39PE/xry8SAoLbpbCMraqlX7kUP6KHOnrlrtvuJLR" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600,800" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Oswald:500,700" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.0/slick/slick.css"/>
